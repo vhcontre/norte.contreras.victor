fb1f8a23e7beb402e667403516767d5d6caecda2
Victor Contreras <vcontreras@unpaz.edu.ar>
2020-05-04 17:50:05 -0300

    Configuración y Creación de sitio bases

---
+++ Solution-Demo/Lppa.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lppa.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "La página de descripción de su aplicación.";
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Su página de contacto.";
+            return View();
+        }
+    }
+}
