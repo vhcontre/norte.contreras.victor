0b2083d04fdd0420e688bb62f67343c3b48568e3
Victor Contreras <vcontreras@unpaz.edu.ar>
2020-05-11 21:44:37 -0300

    Se agregó clase de javascript

---
+++ Solution-Demo/Lppa.WebSite/Controllers/JavaScriptController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lppa.WebSite.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Variables()
+        {
+            return View();
+        }
+        public ActionResult Condicionales()
+        {
+            return View();
+        }
+        
+    }
+}
